a3ec8da15a7f35b67503f1f705a7f149a2903502
shaine <shainilps05@gmail.com>
2023-10-28 20:20:20 +0530

    fixed the minor bug with complex logic(not needed)

--- app.js
+++ app.js
@@ -2,30 +2,30 @@ function navBar(){
 let menu_button=document.getElementById("menu");
 let close_button=document.getElementById("close");
 let navbar=document.querySelector("nav");
+let navbar_list = document.querySelector(".navbar-list");
+let menu_title = document.querySelector(".menu-title");
 let originalPadding = window.getComputedStyle(navbar).padding;
 
-   let navbar_list = document.querySelector(".navbar-list");
-   let menu_title = document.querySelector(".menu-title");
-   let menu_bar = document.querySelector(".menu-bar");
-   let menu_close = document.querySelector(".menu-close");
-
-
 menu_button.addEventListener("click",(event)=>{
-      event.preventDefault(); 
+      event.preventDefault();
 navbar_list.classList.add("tnavbar-list");
 menu_title.classList.add("tmenu-title");
 navbar.style.padding="0";
-menu_button.style.display="none"
-close_button.style.display="block"
+menu_button.classList.remove("menu-bar-display")
+close_button.classList.remove("menu-close-display");
+menu_button.classList.add("not-visible")
+close_button.classList.add("visible")
 })
 
 close_button.addEventListener("click", (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
   navbar_list.classList.remove("tnavbar-list");
   menu_title.classList.remove("tmenu-title");
-  menu_button.style.display = "block";
-  close_button.style.display = "none";
  navbar.style.padding=originalPadding;
+ menu_button.classList.remove("not-visible");
+ close_button.classList.remove("visible");
+menu_button.classList.add("menu-bar-display");
+close_button.classList.add("menu-close-display");
 });
 
 }
