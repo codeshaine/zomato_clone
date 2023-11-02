39a509a2078b4a57b109e0b70762f2d42d87ff96
shaine <shainilps05@gmail.com>
2023-11-02 20:45:57 +0530

    refacored some code and fixed somw bugs

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ function navBar() {
       let navbar = document.querySelector("nav");
       let navbar_list = document.querySelector(".navbar-list");
       let menu_title = document.querySelector(".menu-title");
-      let originalPadding = window.getComputedStyle(navbar).padding;
-      let tempPadding = '0px 0px 48px'
+
 
       menu_button.addEventListener("click", (event) => {
             event.preventDefault();
             navbar_list.classList.add("tnavbar-list");
             menu_title.classList.add("tmenu-title");
-            navbar.style.padding = tempPadding;
             menu_button.classList.remove("menu-bar-display")
             close_button.classList.remove("menu-close-display");
             menu_button.classList.add("not-visible")
@@ -20,7 +18,6 @@ function navBar() {
 
       close_button.addEventListener("click", (event) => {
             event.preventDefault();
-            navbar.style.padding = originalPadding;
             navbar_list.classList.remove("tnavbar-list");
             menu_title.classList.remove("tmenu-title");
 
