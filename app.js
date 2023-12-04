1617ff722040cbba9fd54155818821e5be051c42
shaine <shainilps05@gmail.com>
2023-12-04 21:03:26 +0530

    done with page 1

--- app.js
+++ app.js
@@ -30,3 +30,17 @@ function navBar() {
 }
 navBar();  //wrost way to do this.should have been refactoed..
 
+
+let section_3=document.getElementById("page1-section-3");
+let special_div_h3 = document.getElementById("localities-special");
+
+
+section_3.addEventListener("click",()=>{
+section_3.classList.toggle("see-less");
+
+if (special_div_h3.textContent=="see less") 
+special_div_h3.textContent = "see more";
+else
+special_div_h3.textContent = "see less";
+
+})
