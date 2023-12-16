ce7e51d4836ac8f53b81a045e282df34740f9cdc
shaine <shainilps05@gmail.com>
2023-12-16 22:01:48 +0530

    ok this commit is one fof the best cause finally reduced the code in app.js made little clear how navbar works

--- app.js
+++ app.js
@@ -1,46 +1,30 @@
-function navBar() {
-      let menu_button = document.getElementById("menu");
-      let close_button = document.getElementById("close");
-      let navbar = document.querySelector("nav");
-      let navbar_list = document.querySelector(".navbar-list");
-      let menu_title = document.querySelector(".menu-title");
+function navbar() {
+  let menu_button = document.getElementById("menu");
+  let close_button = document.getElementById("close");
+  let navbar = document.querySelector(".navbar");
 
+  menu_button.addEventListener("click", (event) => {
+    event.preventDefault();
+    navbar.classList.add("squeez");
+  });
 
-      menu_button.addEventListener("click", (event) => {
-            event.preventDefault();
-            navbar_list.classList.add("tnavbar-list");
-            menu_title.classList.add("tmenu-title");
-            menu_button.classList.remove("menu-bar-display")
-            close_button.classList.remove("menu-close-display");
-            menu_button.classList.add("not-visible")
-            close_button.classList.add("visible")
-      })
-
-      close_button.addEventListener("click", (event) => {
-            event.preventDefault();
-            navbar_list.classList.remove("tnavbar-list");
-            menu_title.classList.remove("tmenu-title");
-
-            menu_button.classList.remove("not-visible");
-            close_button.classList.remove("visible");
-            menu_button.classList.add("menu-bar-display");
-            close_button.classList.add("menu-close-display");
-      });
-
+  close_button.addEventListener("click", (event) => {
+    event.preventDefault();
+    navbar.classList.remove("squeez");
+  });
 }
-navBar();  //wrost way to do this.should have been refactoed..
+navbar();
 
+function list() {
+  let section_3 = document.getElementById("page1-section-3");
+  let special_div_h3 = document.getElementById("localities-special");
 
-let section_3=document.getElementById("page1-section-3");
-let special_div_h3 = document.getElementById("localities-special");
+  section_3.addEventListener("click", () => {
+    section_3.classList.toggle("see-less");
 
-
-section_3.addEventListener("click",()=>{
-section_3.classList.toggle("see-less");
-
-if (special_div_h3.textContent=="see less") 
-special_div_h3.textContent = "see more";
-else
-special_div_h3.textContent = "see less";
-
-})
+    if (special_div_h3.textContent == "see less")
+      special_div_h3.textContent = "see more";
+    else special_div_h3.textContent = "see less";
+  });
+}
+list();
